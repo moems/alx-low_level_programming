ebf2857f05eceb366d702864e92a93e5a41d4abc
moems <mohammedgidado@hotmail.com>
2023-03-17 00:02:15 +0000

    I am tayad

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
