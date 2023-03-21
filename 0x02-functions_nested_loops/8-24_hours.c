3b9afa2f7626624274863e4f1a052f1bdb23f807
moems <mohammedgidado@hotmail.com>
2023-03-21 18:38:03 +0000

    8 done

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Print every minute of the day
+ */
+void jack_bauer(void)
+{
+	int h, m;
+	
+	h = 0;
+	while (h < 24)
+	{
+		m = 0;
+		while (m < 60)
+		{
+			if (h > 9)
+			{
+				_putchar('0' + h / 10);
+				_putchar('0' + h % 10);
+			}
+			else
+			{
+				_putchar('0');
+				_putchar('0' + h);
+			}
+
+			_putchar(':');
+
+			if (m > 9)
+			{
+				_putchar('0' + m / 10);
+				_putchar('0' + m % 10);
+			}
+			else
+			{
+				_putchar('0');
+				_putchar('0' + m);
+			}
+			_putchar('\n');
+			m++;
+		}
+		h++;
+	}
+}
