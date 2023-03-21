83e92a6f1c244f555f57dc99c263c9db1f3ee283
moems <mohammedgidado@hotmail.com>
2023-03-21 21:17:25 +0000

    101 last time

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,29 +1,25 @@
 #include "main.h"
 /**
- * main - prints the sum of all natural numbers 
- * that are factors of 3 and 5 below 1024 
+ * main - prints the sum of all natural numbers
+ * that are factors of 3 and 5 below 1024.
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	int a, b, summ, target;
+	int a, summ, target;
 
 	a = 0;
-	b = 0;
 	target = 1024;
 	summ = 0;
 
 	while (a < target)
 	{
-		summ += a;
-		a += 3;
-	}
-
-	while (b < target)
-	{
-		summ += b;
-		b += 5;
+		if (a % 3 == 0 || a % 5 == 0)
+		{
+			summ += a;
+		}
+		a++;
 	}
 
 	printf("%i\n", summ);
