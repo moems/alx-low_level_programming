e15bc318fb59d9efbb1f18c125484092c15360e3
moems <mohammedgidado@hotmail.com>
2023-03-21 21:55:18 +0000

    104 done

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * main - print the first 50 fibonaci numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	unsigned long i, a, b, temp;
+
+	i = 0;
+	a = 0;
+	b = 1;
+
+
+	while (i < 98)
+	{
+		printf("%lu", a + b);
+		if (i != 97)
+			printf(", ");
+		else
+			printf("\n");
+
+		temp = b;
+		b = b + a;
+		a = temp;
+
+		i++;
+	}
+
+	return (0);
+}
