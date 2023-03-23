d11af4737b8e50a4aad2a89222a341e5dcdcf4bd
moems <mohammedgidado@hotmail.com>
2023-03-23 21:09:49 +0000

    5 done

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints the decimal numbers
+ *
+ * Return: Always 0 (Success)
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (i < 10)
+	{
+		j = 1;
+		while (j < 15)
+		{
+			if (j > 9)
+			{
+				_putchar('1');
+			}
+
+			_putchar('0' + j % 10);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
