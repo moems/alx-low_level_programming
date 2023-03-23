61c58a2bd4e9781027db2c4b8548f2ea0c14d8b3
moems <mohammedgidado@hotmail.com>
2023-03-23 22:33:59 +0000

    10 done

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a right angle triangle
+ * @size:  the number of times the character # should be printed
+ */
+void print_triangle(int size)
+{
+	int i, j, n;
+
+	n = size;
+	i = 0;
+	while (i < n)
+	{
+		j = 0;
+		while (j < n - i)
+		{
+			_putchar(' ');
+			j++;
+		}
+
+		j = 0;
+		while (j < i + 1)
+		{
+			_putchar('#');
+			j++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
