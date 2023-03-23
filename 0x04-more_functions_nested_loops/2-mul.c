c1f8c3dc0b68b2ee04f95873ee07b069fcdd8cbe
moems <mohammedgidado@hotmail.com>
2023-03-23 20:35:13 +0000

    2 done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - determines if the character c is a digit
+ * @a: int to be multiplied
+ * @b: int to be multiplied
+ *
+ * Return: the product of `a` and `b`
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
