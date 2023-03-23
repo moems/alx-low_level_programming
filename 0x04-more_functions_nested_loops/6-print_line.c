4899782c2f096f2094bd0d85993140a55c03a6aa
moems <mohammedgidado@hotmail.com>
2023-03-23 21:16:39 +0000

    6 done

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - Prints n number of underscores
+ * @n:  the number of times the character _ should be printed
+ *
+ * Return: Always 0 (Success)
+ */
+void print_line(int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
