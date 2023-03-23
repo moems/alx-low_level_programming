0f0f31a51164cada7e9a4e9b80a48a492c8d6636
moems <mohammedgidado@hotmail.com>
2023-03-23 21:53:40 +0000

    8 done

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a square
+ * @n:  the number of times the character # should be printed
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	i = 0;
+	while (i < n)
+	{
+		j = 0;
+		while (j < #)
+		{
+			_putchar('#');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
