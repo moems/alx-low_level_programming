3e57b913dcda467f7e3dfee68c3e37287cd66173
moems <mohammedgidado@hotmail.com>
2023-04-11 20:04:00 +0000

    100 done

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * str_concat - Concactinates two strings
+ * @ac: Argument count.
+ * @av: Argument Variable.
+ *
+ * Return: Null if size is 0 or pointer to the newly fomred string.
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, k, count;
+	char *cact;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	count = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			count++;
+		}
+		count++;
+	}
+
+	cact = malloc((count + 1) * sizeof(char));
+	if (cact == NULL)
+		return (NULL);
+
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			cact[k++] = av[i][j];
+		}
+		cact[k++] = '\n';
+	}
+
+	cact[k] = '\0';
+
+	return (cact);
+}
