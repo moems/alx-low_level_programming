884febe48275e0c2b03afe12e4f97084f9718856
moems <mohammedgidado@hotmail.com>
2023-04-13 14:30:14 +0000

    2 done again

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	tmp = malloc(nmemb * sizeof(size));
+	tmp = malloc(nmemb * size);
 
 	if (tmp == NULL)
 		return (NULL);
