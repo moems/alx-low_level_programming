745e5883eff5450bb94b9ddde9a305d45a538234
moems <mohammedgidado@hotmail.com>
2023-04-17 16:27:50 +0000

    3 and 4 again

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,18 +1,6 @@
 #ifndef ABS_FUNC
 #define ABS_FUNC
 
-/**
- * ABS - Determines the absolute value of x.
- * @x: the number to be converted.
- *
- * Return: The absolute value of x.
- */
-int ABS(int x)
-{
-	if (x < 0)
-		return (-1 * x);
-	else
-		return (x);
-}
+#define ABS(x) ((x) < (0) ? (-1 * x) : (x))
 
 #endif /* #ifndef ABS_FUNC */
