e130e5f02f6d18e601aa43f8a7089542c28a20b0
moems <mohammedgidado@hotmail.com>
2023-04-19 19:02:37 +0000

    First commit of the function pointers project. task 0 done.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,9 @@
+/**
+ * print_name - function prints a name using a function passed as an argument.
+ * @name: Name to print.
+ * @f:    Function to be used to print name.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
