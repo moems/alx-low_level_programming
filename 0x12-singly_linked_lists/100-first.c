fabecc91f42f75639bd6256df052b0dbfd65770a
moems <mohammedgidado@hotmail.com>
2023-04-27 23:31:16 +0000

    100 done

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * before - Performs an action before main is executed.
+ */
+void before(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
