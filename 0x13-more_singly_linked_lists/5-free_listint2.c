89f503a4cff153a2f60d057b98a4cd10328948ff
moems <mohammedgidado@hotmail.com>
2023-04-30 00:36:39 +0000

    5 done

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees allocated memories.
+ * @head: head of a linked list.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (!head)
+		return;
+
+	while (*head)
+	{
+		tmp = *head;
+		*head = tmp->next;
+		free(tmp);
+	}
+	return;
+}
