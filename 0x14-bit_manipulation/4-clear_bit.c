2c955f24229df9db91855b0ee86d36caedb5039a
moems <mohammedgidado@hotmail.com>
2023-05-04 21:50:29 +0000

    5 done

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * clear_bit - change the bit at an index to 1.
+ * @n: pointer to int to be changed.
+ * @index: index of bit to be changed.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*n = *n & ~(1 << index);
+	return (1);
+}
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bits - count number of bits in n that need to be converted to get m.
+ * @n: an unsigned int long integer number 
+ * @m: an unsigned int long integer number
+ *
+ * Return: The number of bits that need to be fliped.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int i, count = 0;
+	unsigned long int result;
+
+	result = n ^ m;
+
+	for (i = 0; i < 64; i++)
+	{
+		if ((result >> i) & 1)
+			count++;
+	}
+
+	return (count);
+}
