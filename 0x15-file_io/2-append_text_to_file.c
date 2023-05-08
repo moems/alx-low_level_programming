8c7e4a70dd1dd2314261a3863ad40354f6ac8b8d
moems <mohammedgidado@hotmail.com>
2023-05-08 20:54:16 +0000

    2 done

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	while (text_content[i])
+	while (text_content && text_content[i])
 		i++;
 
 	fd = open(filename, O_WRONLY | O_APPEND);
