c81dbae6b7f8c6caf98ccca029194cfe7f63cc7e
moems <mohammedgidado@hotmail.com>
2023-05-08 20:43:54 +0000

    I give up on that one silly mark

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,13 +17,10 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		while (text_content[i])
-			i++;
-	}
+	while (text_content[i])
+		i++;
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (0);
