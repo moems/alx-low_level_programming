1987029436dac3f1411a392d1ee037aa8da07aff
moems <mohammedgidado@hotmail.com>
2023-05-08 20:35:04 +0000

    1 one more time

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	while (text_content[i])
+	while (text_content != NULL && text_content[i])
 		i++;
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
