7347d685e70f03d7f78e06a3c491b97e240c460c
moems <mohammedgidado@hotmail.com>
2023-05-08 20:49:49 +0000

    1 not working :(

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	while (text_content[i])
+	while (text_content && text_content[i])
 		i++;
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
